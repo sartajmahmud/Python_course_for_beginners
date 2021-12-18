91c026bdc6185fbb4c388a204060ec9caae57ca0
Sartaj Mahmud <oishik1999@gmail.com>
2021-12-18 21:30:05 +0600

    Fifth class on solving nested loops and conditions practice Problems from previous class.

--- main.py
+++ main.py
@@ -1,110 +1,73 @@
-"""
-///Nested Conditions
-Example problem:
-    find leap year using nested condition
+# patternLength = int(input())
 
-solution:
-    x=int(input())
+# for column in range(1, patternLength+1):
+#     for row in range(column):
+#         print('*', end='')
+#     print()
 
-    if x%4==0:
-        if x%100==0:
-            if x%400==0:
-                print("leap year")
-            else:
-                print("not leap year")
-        else:
-            print("leap year")
-    else:
-        print("not leap year")
+# for column in range(1, patternLength+1):
+#     for spaceRow in reversed(range(1,(patternLength+1)-column)):
+#         print(' ', end='')
+#     for row in range(column):
+#         print('*', end='')
+#     print()
 
-
-Practice problems:
-P1:
-*****
-  *
-  *
-  *
-*****
-
-P2:
-*****
-*   *
-*   *
-*   *
-*****
-
-P3:
-*   *
-*   *
-*****
-*   *
-*   *
-
-"""
+# for row in range(1,patternLength+1):
+#     if row==1 or row==patternLength:
+#         for col in range(1,patternLength+1):
+#             print("*",end="")
+#     else:
+#         for col in range(1, patternLength + 1):
+#             if col == (patternLength + 1)/2:
+#                 print("*", end="")
+#             else:
+#                 print(" ",end="")
+#     print()
 
 
+#neelavro problem
+# for i in range(1, patternLength+1):
+#     print(i,end='')
+# for i in reversed(range(1, patternLength)):
+#     print(i,end='')
 
-
-"""
-
-//Nested Loops
-
-    column -> 1
-    row -> 0
-    output : *
-
-    column -> 2
-    row -> 0, 1
-    output : **
-
-    column -> 3
-    row -> 0, 1, 2
-    output : ***
-    
-    column -> 4
-    row -> 0, 1, 2, 3
-    output : ****
-    
-    column -> 5
-    row -> 0, 1, 2, 3, 4
-    output : *****
-
-    example 1:
-    
-    *
-    **
-    ***
-    ****
-    *****
-    
-    solution :
-    for column in range(1,6):
-        for row in range(column):
-            print('*', end='')
+for row in range(1, 6):
+    for space in range(1,6 - row):
+        print("_", end="")
+    for star in range(1, row+1):
+        print("*", end='')
     print()
 
-    //Practice problems for nested loop
-    practice 1:
-    *****
-    ****
-    ***
-    **
-    *
 
-    practice 2:
-    *****
-     ****
-      ***
-       **
-        *
 
-    practice 3:
-        *
-       **
-      ***
-     ****
-    *****
+    """
+    5
+    123454321
     
-
-
-"""
+    
+    ***** -> 1
+    *   *
+    *   *
+    *   *
+    ***** -> 5
+    
+    
+    practice 3:
+        **
+       ****
+      ******
+     ********
+    **********
+    **********
+     ********
+      ******
+       ****
+        **
+    
+     practice 1:
+    *****
+     ****
+      ***
+       **
+        *
+    """
