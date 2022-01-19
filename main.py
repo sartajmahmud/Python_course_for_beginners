d790a04929ab119dcd52c9f2e89ff21726b140ed
Sartaj Mahmud <oishik1999@gmail.com>
2022-01-19 22:01:14 +0600

    Solved a problem on Dictionary

--- main.py
+++ main.py
@@ -1,84 +1,73 @@
-# """
-# Encapsulcation
-#
-# 1. Getter functions/methods
-# 2. Setter function/methods
-# """
-#
-#
-# class Person:
-#
-#     def __init__(self,name, address, phonenumber = None):
-#         self.name = name
-#         self.address = address
-#         self.phoneNumber = phonenumber
-#
-#
-#     def printData(self):
-#         print("Name is the object : ",self.name)
-#         print("Address is the object : ",self.address)
-#         print("PhoneNumber is the object : ",self.phoneNumber)
-#
-#     def getPhoneNumber(self):
-#         return self.phoneNumber
-#
-#     def setPhoneNumber(self, newPhoneNumber):
-#         self.phoneNumber = newPhoneNumber
-#
-#
-# person1 = Person('saaaad','kamala',123)
-# # person2 = Person('sajjad','lalmaa',123465)
-#
-# #
-# # person1.phoneNumber = 123456789
-# # person2.phoneNumber = 987654321
-#
-# print(person1.getPhoneNumber())
-#
-# person1.setPhoneNumber(654321)
-#
-# print(person1.getPhoneNumber())
-#
-# # Person('Umama','Dhanmondi',123).printData()
-# #
-# # person1.printData()
-# #
-# # person2.printData()
-#
+inputNumber = 0
+outputDict = {}
+
+while(True):
+    inputNumber = inputNumber + 1
+
+    userInput = input() #user input -> "OMEGA 10000" / "STOP"
+
+    if userInput == 'STOP':
+        break
+    else:
+        inputParts = userInput.split(' ') # if input -> "OMEGA 10000" -> splited list will be -> ['OMEGA','10000']
+
+        firstTwoLetter = inputParts[0][0:2]
+
+        if inputParts[0] in outputDict:   # searches for previous keys with the new input brand name
+
+            outputDict[inputParts[0]].append(str(inputNumber)+"_"+firstTwoLetter+'_'+inputParts[1])
+        else:
+            outputDict[inputParts[0]] = []
+            outputDict[inputParts[0]].append(str(inputNumber)+"_"+firstTwoLetter+'_'+inputParts[1])
+
+print(outputDict)
 
 
-# x = 10
+# dictX = {}
+# dictX['KOTKA'] = []
 #
-# def getX():
-#     return x
+# dictX['KOTKA'].append('1_KO_1000')
+# dictX['KOTKA'].append('2_KO_3000')
 #
-# def setX(value):
-#     global x
-#     x = value
-#
-#
-# setX(200)
-#
-# print(getX())
+# print(dictX)
 
-import Person
+"""Write a python program that will take input from the user until the user
+gives “STOP” as input. In every line the user will provide input as follow:
+[Brand Name] [PRICE]
+Your task is to create a dictionary where the brand’s name will be key and
+the value will be a list of that brand’s watches ID. ID generate process:
+inputno_FirstTwoLetterOfBrand_Price. Finally print the dictionary.
 
-# Method Overloading
+Sample Input 1:
+OMEGA 10000
+TITAN 5000
+TITAN 3000
+CREDENCE 150000
+OMEGA 12000
+CREDENCE 90000
+STOP
+Sample Output 1:
+{
+'OMEGA': ['1_OM_10000', '5_OM_12000'], 
+'TITAN':['2_TI_5000', '3_TI_3000'], 
+'CREDENCE': ['4_CR_150000', '6_CR_90000']
+} 
 
-person1 = Person.Person('Sartaj', 'Uttara')
-person2 = Person.Person('Neelavro', 'Shankar', 7689)
 
-# print(person1.name)
-# print(person1.address)
-# print(person1.phoneNumber)
-#
-# print(person2.name)
-# print(person2.phoneNumber)
+ OMEGA 1000 its a 1st input -> 1               The value of inputCounter is now 1          inputCounter = inputCounter + 1
+
+ [0]-> OMEGA                                   Found the Brand name "OMEGA"                userInput.split(' ')[0]
+ [1] -> 1000                                   FOUND the price of the watch                userInput.split(' ')[1]
+
+ first 2 letter -> OM                          FOUND the first two letter of the brand     userInput.split(' ')[0][0:2]
+
+ 1_OM_1000                                     Processed output String                     str(inputCounter)+'_'+userInput.split(' ')[0][0:2]+'_'+userInput.split(' ')[1]
+
+ {'OMEGA' : []}                                dictX[userInput.split(' ')[0]] = []
+ {'OMEGA' : ['1_OM_1000']}                     dictX[userInput.split(' ')[0]].append(str(inputCounter)+'_'+userInput.split(' ')[0][0:2]+'_'+userInput.split(' ')[1])
 
 
-def addValues(num1, num2, num3=0):
-    return num1+num2+num3
 
-print(addValues(5,5))
-print(addValues(5,5,10))
+
+"""
 
