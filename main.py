d39ce8538a8166fcfd6cc7156214db16500b7a14
Sartaj Mahmud <oishik1999@gmail.com>
2022-01-29 21:55:22 +0600

    structured programming project

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
-keypad = {
-    1:'.,?!:',
-    2:'ABC',
-    3:'DEF',
-    4:'GHI',
-    5:'JKL',
-    6:'MNO',
-    7:'PQRS',
-    8:'TUV',
-    9:'WXYZ',
-    0:' ',
-}
+"""
+1. takes a user input for number of line to print patterns
+2. takes a user input for number of line to print a diamond of that lines (only odd inputs)
+3. a guessing game
+4. opens a address book (add, delete, update, and view all addresses)
+5. finds factorial of inputted number
+6. takes a input and finds addition of a sequence (1,1, 2, 4, 8, 16.........n)
+7. takes a input and finds addition of a sequence (1,3, 5, 7, 9, 11.........n)
+8. takes a input and finds addition of a sequence (1,4, 9, 16, 25, 36.........n)
 
-userInput = "Hello, World!".upper()
-index = 0
-outputString = ''
+"""
+
+
+
+def app1():
+    print('app1')
+def app2():
+    print('app2')
+def app3():
+    print('app3')
+
+def app4():
+    print('app4')
+
+def app5():
+    print('app5')
+
+
+def printMenu():
+    print('**************')
+    print('Press 1 for app1')
+    print('Press 2 for app2')
+    print('Press 3 for app3')
+    print('Press 4 for app4')
+    print('Press 5 for app5')
+    print('Press 0 for Exit')
+    print('**************')
+
 
 while(True):
-    for i in range(10):
-        for j in range(len(keypad[i])):
-            if keypad[i][j] == userInput[index]:
-                for k in range(j+1):
-                    outputString += str(i)
+    printMenu()
+    menuInput = int(input('Enter your choice : '))
 
-    index+=1
-
-    if index == len(userInput):
-        break
-
-
-print(outputString)
-
-
-
-# dict1={'1':'.,?!:','2':'ABC','3':'DEF','4':'GHI','5':'JKL','6':'MNO','7':'PQRS','8':'TUV','9':'WXYZ','0':' '}
-# str1=input()
-# str2=str1.upper()
-# str3=""
-# for i in range(len(str2)):
-#     for j,value in dict1.items():
-#         if str2[i] in value:
-#             for x in range(1,value.find(str2[i])+2):
-#                 str3=str3+j
-# print(str3)
+    if(menuInput == 1):
+        app1()
+    elif(menuInput == 0):
+        print('Exited')
+        break;
